d918c0e72512cf4d39c3acd3192db1cb1880abc7
divyam <149057810+Divyam56@users.noreply.github.com>
2025-03-23 16:34:06 +0530

    Create fifth.py

---
+++ fifth.py
@@ -0,0 +1,7 @@
+n = int(input())
+if n%2 == 0 and 2 <= n <= 5:
+    print("it is even bbg and in between 2 and 5")
+elif n%2 == 0 and 10 <= n <= 10:
+    print("it is even bbg and in between 10 and 20")
+else:
+    print("it is odd bbg")
